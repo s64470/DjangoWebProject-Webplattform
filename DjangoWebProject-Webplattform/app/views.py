8bd63e51b9c597f3695ef1795bd6fdce8d111945
s64470 <s64470@bht-berlin.de>
2024-03-04 22:19:54 +0100

    Add register_view.

--- DjangoWebProject-Webplattform/app/views.py
+++ DjangoWebProject-Webplattform/app/views.py
@@ -4,7 +4,9 @@ Definition of views.
 
 from datetime import datetime
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpRequest
+
 
 def home(request):
     """Renders the home page."""
@@ -13,10 +15,11 @@ def home(request):
         request,
         'app/index.html',
         {
-            'title':'Home Page',
-            'year':datetime.now().year,
+            'title': 'Home Page',
+            'year': datetime.now().year,
         }
     )
+
 
 def contact(request):
     """Renders the contact page."""
@@ -25,11 +28,12 @@ def contact(request):
         request,
         'app/contact.html',
         {
-            'title':'Contact',
-            'message':'Your contact page.',
-            'year':datetime.now().year,
+            'title': 'Contact',
+            'message': 'Your contact page.',
+            'year': datetime.now().year,
         }
     )
+
 
 def about(request):
     """Renders the about page."""
@@ -38,8 +42,19 @@ def about(request):
         request,
         'app/about.html',
         {
-            'title':'About',
-            'message':'Your application description page.',
-            'year':datetime.now().year,
+            'title': 'About',
+            'message': 'Your application description page.',
+            'year': datetime.now().year,
+        }
+    )
+
+
+def register_view(request):
+    """Renders the register page."""
+    form = UserCreationForm()
+    return render(
+        request, 'app/register.html',
+        {
+            'form': form
         }
     )
