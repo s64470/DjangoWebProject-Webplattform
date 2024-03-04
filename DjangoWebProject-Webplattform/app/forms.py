80e0680612fa60c9343cdbfef661aa97bfe5dd7b
s64470 <s64470@bht-berlin.de>
2024-03-04 23:40:29 +0100

    Add RegisterUserForm.

--- DjangoWebProject-Webplattform/app/forms.py
+++ DjangoWebProject-Webplattform/app/forms.py
@@ -2,17 +2,59 @@
 Definition of forms.
 """
 
+from dataclasses import fields
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 class BootstrapAuthenticationForm(AuthenticationForm):
     """Authentication form which uses boostrap CSS."""
-    username = forms.CharField(max_length=254,
-                               widget=forms.TextInput({
-                                   'class': 'form-control',
-                                   'placeholder': 'User name'}))
-    password = forms.CharField(label=_("Password"),
-                               widget=forms.PasswordInput({
-                                   'class': 'form-control',
-                                   'placeholder':'Password'}))
+    username = forms.CharField(
+        max_length=254,
+        widget=forms.TextInput(
+            {
+                'class': 'form-control',
+                'placeholder': 'User name'
+            }))
+
+    password = forms.CharField(
+        label=_("Password"),
+        widget=forms.PasswordInput(
+            {
+                'class': 'form-control',
+                'placeholder': 'Password'
+            }))
+
+
+class RegisterUserForm(UserCreationForm):
+    """Registration form which uses boostrap CSS."""
+    email = forms.EmailField()
+    first_name = forms.CharField(
+        max_length=254,
+        widget=forms.TextInput(
+            {
+                'class': 'form-control',
+                'placeholder': 'First name'
+            }))
+    last_name = forms.CharField(
+        max_length=254,
+        widget=forms.TextInput(
+            {
+                'class': 'form-control',
+                'placeholder': 'Last name'
+            }))
+    user_name = forms.CharField(
+        max_length=254,
+        widget=forms.TextInput(
+            {
+                'class': 'form-control',
+                'placeholder': 'User name'
+            }))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email',
+                  'password', 'repeat_password')
